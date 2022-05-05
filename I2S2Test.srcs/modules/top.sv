d0a7ac89d47573226eb05d2e500261c8c737c933
AndrewJones-PSU <aej5218@psu.edu>
2022-05-05 04:58:00 -0400

    more clock signal renaming because I am indecisive

--- I2S2Test.srcs/modules/top.sv
+++ I2S2Test.srcs/modules/top.sv
@@ -2,14 +2,14 @@
 // the I2S2 pmod and connects it to the appropriate rx/tx modules and filters.
 
 module Top (
-		input bclk, // 100 MHz clock from the Mimas A7 board
+		input boardclk, // 100 MHz Board clock from the Mimas A7 board
 		input linData, // data in from the I2S2 pmod
 
-		output linSclk, // Serial clock out to the I2S2 pmod
+		output linBclk, // Bit clock out to the I2S2 pmod
 		output linLr, // LR/word select signal out to the I2S2 pmod
 		output linMclk, // Master Clock out to the I2S2 pmod
 		output loutData, // data out to the I2S2 pmod
-		output loutSclk, // Serial clock out to the I2S2 pmod
+		output loutBclk, // Bit clock out to the I2S2 pmod
 		output loutLr, // LR/word select signal out to the I2S2 pmod
 		output loutMclk // Master Clock out to the I2S2 pmod
 	);
